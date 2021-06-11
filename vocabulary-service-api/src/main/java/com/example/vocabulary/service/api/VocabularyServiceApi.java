651dd36fc5d3987c89e37219a7c4121ee2be43bb
Vijay Kumar Rajput <vijaykrajpoot@gmail.com>
2021-06-10 23:48:25 -0700

    Swagger doc

--- vocabulary-service-api/src/main/java/com/example/vocabulary/service/api/VocabularyServiceApi.java
+++ vocabulary-service-api/src/main/java/com/example/vocabulary/service/api/VocabularyServiceApi.java
@@ -1,16 +1,6 @@
 package com.example.vocabulary.service.api;
 
-import com.example.vocabulary.service.api.config.api.model.Vocabulary;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-
-import javax.validation.Valid;
-import java.util.Optional;
-
 public interface VocabularyServiceApi extends VocabularyApi {
-    @Override
-    default ResponseEntity<Vocabulary> getVocabularybyType(@Valid Optional<String> type) {
-        return null;
-    }
+
+
 }
--- vocabulary-service-server/src/main/java/com/example/vocabulary/service/server/controllers/VocabularyController.java
+++ vocabulary-service-server/src/main/java/com/example/vocabulary/service/server/controllers/VocabularyController.java
@@ -1,14 +1,18 @@
 package com.example.vocabulary.service.server.controllers;
 
 import com.example.vocabulary.service.api.VocabularyServiceApi;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.vocabulary.service.api.config.api.model.Word;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class VocabularyController implements VocabularyServiceApi {
 
-    @RequestMapping("/")
-    public String root() {
-        return "Vocabulary Service";
+    @Override
+    public ResponseEntity<List<Word>> getVocabularyByType( Optional<String> type) {
+        return null;
     }
 }
